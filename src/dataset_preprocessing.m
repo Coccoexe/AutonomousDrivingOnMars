5e469c79e9d836e5770f80177fed6fd7351eb430
Alessio Cocco <ale11022000@gmail.com>
2024-03-08 17:26:46 +0100

    preprocessing file

---
+++ src/dataset_preprocessing.m
@@ -0,0 +1,59 @@
+clear all
+clc
+
+% CONFIGURATION
+dataset_folder = 'dataset/ai4mars-dataset-merged-0.1/msl';
+train_folder   = strcat(dataset_folder, '/images/edr/train');
+test_folder    = strcat(dataset_folder, '/images/edr/test');
+ltrain_folder  = strcat(dataset_folder, '/labels/train');
+ltest_folder   = strcat(dataset_folder, '/labels/test/masked-gold-min2-100agree');
+
+dataset_name = 'dataset/ai4mars';
+image_size  = [256, 256];
+
+% DATASET FOLDERS GENERATION
+delete(strcat(dataset_name, '/*'));
+mkdir(dataset_name);
+mkdir(strcat(dataset_name, '/images'));
+mkdir(strcat(dataset_name, '/images/train'));
+mkdir(strcat(dataset_name, '/images/test'));
+mkdir(strcat(dataset_name, '/labels'));
+mkdir(strcat(dataset_name, '/labels/train'));
+mkdir(strcat(dataset_name, '/labels/test'));
+
+% LOAD IMAGES
+imds_train = imageDatastore(train_folder);
+imds_test  = imageDatastore(test_folder);
+imdsl_train = imageDatastore(ltrain_folder);
+imdsl_test  = imageDatastore(ltest_folder);
+
+% PREPROCESS IMAGES
+resize = @(x) imresize(x, image_size);
+duplicate = @(x) cat(3, x, x, x);
+imds_train.ReadFcn = @(x) duplicate(resize(imread(x)));
+imds_test.ReadFcn  = @(x) duplicate(resize(imread(x)));
+imdsl_train.ReadFcn = @(x) resize(imread(x));
+imdsl_test.ReadFcn  = @(x) resize(imread(x));
+
+% SAVE IMAGES
+disp('Saving Train Images...');
+for i = 1:length(imds_train.Files)
+    img = read(imds_train);
+    imwrite(img, strcat(dataset_name, '/images/train/', num2str(i), '.png'));
+end
+disp('Saving Test Images...');
+for i = 1:length(imds_test.Files)
+    img = read(imds_test);
+    imwrite(img, strcat(dataset_name, '/images/test/', num2str(i), '.png'));
+end
+disp('Saving Train Labels...');
+for i = 1:length(imdsl_train.Files)
+    img = read(imdsl_train);
+    imwrite(img, strcat(dataset_name, '/labels/train/', num2str(i), '.png'));
+end
+disp('Saving Test Labels...');
+for i = 1:length(imdsl_test.Files)
+    img = read(imdsl_test);
+    imwrite(img, strcat(dataset_name, '/labels/test/', num2str(i), '.png'));
+end
+disp('Done!');
--- src/deeplabv3Plus.m
+++ src/deeplabv3Plus.m
@@ -2,34 +2,31 @@ clear all
 clc
 
 % CONFIGURATION
-dataset_folder = 'dataset/ai4mars-dataset-merged-0.1/msl';
-train_folder   = 'images/edr/train';
-test_folder    = 'images/edr/test';
-ltrain_folder  = 'labels/train';
-ltest_folder   = 'labels/test/masked-gold-min2-100agree';
+dataset_folder = 'dataset/ai4mars';
+train_folder   = strcat(dataset_folder, '/images/train');
+test_folder    = strcat(dataset_folder, '/images/test');
+ltrain_folder  = strcat(dataset_folder, '/labels/train');
+ltest_folder   = strcat(dataset_folder, '/labels/test');
 
 image_size  = [256, 256, 3];
 numClasses  = 5;
-divideRatio = 0.85; % #images on train, validation is 1-divideRation
+divideRatio = 0.8; % #images on train, validation is 1-divideRation
 
 % TAKES TRAIN IMAGES AND DIVIDES INTO TRAIN AND VALIDATION
-images = dir(fullfile(dataset_folder, train_folder, '*.jpg'));
+images = dir(fullfile(train_folder, '*.png'));
 images = {images.name};
-images = cellfun(@(x) x(1:end-4), images, 'UniformOutput', false);    % names without extension
 images = images(randperm(length(images)));                            % shuffle images
 train  = images(1:round(divideRatio*length(images)));                 % train images
 val   = images(round(divideRatio*length(images))+1:end);              % validation images
 
 % DATASTORES
-img_preproc = @(img) imresize(cat(3, imread(img), imread(img), imread(img)), image_size(1:2));
-imds_train = imageDatastore(strcat(fullfile(dataset_folder, train_folder, train),'.jpg'), "ReadFcn", img_preproc);
-imds_val   = imageDatastore(strcat(fullfile(dataset_folder, train_folder, val),'.jpg'), "ReadFcn", img_preproc);
+imds_train = imageDatastore(fullfile(train_folder, train));
+imds_val   = imageDatastore(fullfile(train_folder, val));
 
 classes = ["soil","bedrock","sand","bigRock","noLabel"];
 labelIDs = [0, 1, 2, 3, 255];
-label_preproc = @(img) imresize(imread(img), image_size(1:2));
-pxds_train = pixelLabelDatastore(strcat(fullfile(dataset_folder, ltrain_folder, train),'.png'), classes, labelIDs, "ReadFcn", label_preproc);
-pxds_val   = pixelLabelDatastore(strcat(fullfile(dataset_folder, ltrain_folder, val),'.png'), classes, labelIDs, "ReadFcn", label_preproc);
+pxds_train = pixelLabelDatastore(fullfile(ltrain_folder, train), classes, labelIDs);
+pxds_val   = pixelLabelDatastore(fullfile(ltrain_folder, val), classes, labelIDs);
 
 train_cds = combine(imds_train,pxds_train);
 val_cds   = combine(imds_val,pxds_val);
