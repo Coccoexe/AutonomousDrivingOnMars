18ea4bdc4af1e6f30b97b014b2bc6d36618a3df4
Alessio Cocco <ale11022000@gmail.com>
2024-08-12 18:28:55 +0200

    dice loss, missing gradient

--- src/supervised/depth/trainnet/modelLoss.m
+++ src/supervised/depth/trainnet/modelLoss.m
@@ -1,23 +1,31 @@
-function loss = (Y, T)
-
-    [~, max_idx] = max(Y, [], 3);
-    % Create a matrix of zeros with the same size as A
-    Y = zeros(size(Y));
-    % Set the positions of the maximum values to 1 in the corresponding channel
-    for i = 1:size(Y,3)
-        % Logical mask where the maximum occurs in the i-th channel
-        mask = (max_idx == i);
-        % Set those positions to 1 in the corresponding channel of B
-        Y(:,:,i) = mask;
-    end
-
-    % Compute the Generalized Dice loss (assuming you have a function for it)
+function loss = modelLoss(Y, T)
     
-    dice_loss = zeros(1,size(Y,3));
-    for  K = 1:(size(O,3))
-         dice_loss(K)= 1 - generalizedDice(single(Y(:,:,K)), single(T(:,:,K)));
-    end
+    losses = zeros(1,size(Y,4));
 
-    % Compute the final loss as mean of 1 - dice loss
-    loss = mean(dice_loss);
+    for j=1:size(Y,4)
+        Y_j = squeeze(Y(:,:,:,j));
+        T_j = squeeze(T(:,:,:,j));
+
+        [~, max_idx] = max(Y_j, [], 3);
+        % Create a matrix of zeros with the same size as A
+        Y_j(:) = 0;
+        % Set the positions of the maximum values to 1 in the corresponding channel
+        for i = 1:size(Y_j,3)
+            % Logical mask where the maximum occurs in the i-th channel
+            mask = (max_idx == i);
+            % Set those positions to 1 in the corresponding channel of B
+            Y_j(:,:,i) = mask;
+        end
+    
+        % Compute the Generalized Dice loss (assuming you have a function for it)
+        
+        dice_loss = zeros(1,size(Y_j,3));
+        for  K = 1:(size(Y_j,3))
+             dice_loss(K)= 1 - generalizedDice(Y_j(:,:,K), T_j(:,:,K));
+        end
+    
+        % Compute the final loss as mean of 1 - dice loss
+        losses(j) = mean(dice_loss);
+    end
+    loss = mean(losses);
 end
